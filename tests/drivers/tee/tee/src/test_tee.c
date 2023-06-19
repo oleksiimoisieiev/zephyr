ce7b378a365cb882767684a3608dd0ff0a5b7966
Mykola Kvach <mykola_kvach@epam.com>
2023-06-19 09:30:24 +0300

    drivers: tee: optee: add support of shared memory cmds
    Add support of shared memory register/unregister to optee driver.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>

--- tests/drivers/tee/tee/src/test_tee.c
+++ tests/drivers/tee/tee/src/test_tee.c
@@ -44,8 +44,7 @@ static int test_tee_invoke_func(const struct device *dev, struct tee_invoke_func
 	return 0;
 }
 
-static int test_tee_shm_register(const struct device *dev, void *addr, size_t size, uint32_t flags,
-				 struct tee_shm *shm)
+static int test_tee_shm_register(const struct device *dev, struct tee_shm *shm)
 {
 	printk("%s %d\n", __func__, __LINE__);
 	return 0;
